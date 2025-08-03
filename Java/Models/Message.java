e259083282b1b15053ed4944b16134cdb422a2b0
Sameer Butt <146714246+Spidy61163@users.noreply.github.com>
2025-08-03 22:36:17 +0500

    Create Message.java

---
+++ Java/Models/Message.java
@@ -0,0 +1,66 @@
+package pk.edu.itu.bsai23023.chatring.Models;
+
+public class Message {
+    private String messageId, message, senderId, imageUrl;
+    private long timestamp;
+    private int reaction = -1;
+
+    public Message() {
+        this.reaction = -1;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public Message(String message, String senderId, long timestamp) {
+        this.message = message;
+        this.senderId = senderId;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(int reaction) {
+        this.reaction = reaction;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
