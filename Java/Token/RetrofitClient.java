4b9d3f005500bb5dab289dc7efe072c1d56b45ce
Sameer Butt <146714246+Spidy61163@users.noreply.github.com>
2025-08-03 22:43:16 +0500

    Create RetrofitClient.java

---
+++ Java/Token/RetrofitClient.java
@@ -0,0 +1,19 @@
+package pk.edu.itu.bsai23023.chatring.Token;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://your-ngrok-url.ngrok-free.app/";
+
+    public static Retrofit getInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
